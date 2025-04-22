7492692b3a4fe824b97979a2cf24124f15a9f05c
Попов Григорий <popovgp@yandex.ru>
2025-04-22 21:59:45 +0300

    новый файл:    A0.c 	новый файл:    A1.c 	новый файл:    A2.c 	удалено:       main.c

---
+++ hw1/A2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+	printf("   *\n");
+	printf("  ***\n");
+	printf(" *****\n");
+	printf("*******\n");
+	printf(" HH0HH\n");
+	printf(" ZZZZZ\n");
+	return 0;
+}
